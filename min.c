9a0abfe630b514fc1490f73b812cdd863ba819a1
dhvani3289 <123374029+dhvani3289@users.noreply.github.com>
2023-01-26 17:16:10 +0530

    Add files via upload

---
+++ min.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	int a,b;
+	printf("Enter A :");
+	scanf("%d",&a);
+	
+	printf("Enter B :");
+	scanf("%d",&b);
+	
+	if(a<b){
+		printf("A is minimum");
+	}
+	else{
+		printf("B is minimum");
+	}
+	
+	
+}
---
+++ neutral.c
@@ -0,0 +1,13 @@
+
+main(){
+	int n;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	
+		if(n==0){
+		printf("The number is neutral");
+	}
+	else{
+		printf("Invalid Number!!");
+	}
+}
